e352ea38cb5a7a02906558013e1ac638b288a49c
bigJiaLuo <2458681060@qq.com>
2020-06-19 15:49:26 +0800

    图的深度优先遍历算法应用

--- Map/Structrue/AdjacencyList.cpp
+++ Map/Structrue/AdjacencyList.cpp
@@ -71,7 +71,36 @@ void CreateALGraph(GraphAdjList *G)
     }
 }
 
-bool visited[MAXV];//标识此顶点是否被访问
+void CreateALGraph1(GraphAdjList *G)
+{
+    int i, j, k; //i为起点，j为终点
+    EdgeNode *e; //临时保存新邻接点
+    printf("请输入无向图的顶点个数，边数：");
+    scanf("%d %d", &G->numVertex, &G->numEdges);
+    //初始化顶点表
+    for (i = 0; i < G->numVertex; i++)
+    {
+
+        printf("输入第%d个顶点编号:", i + 1);
+        scanf("%d", &G->adjList[i].data);
+        G->adjList[i].firstedg = NULL; //边表置为空
+    }
+    //创建边，建立边表
+    for (k = 0; k < G->numEdges; k++)
+    {
+        printf("请输入（vi，vj）上顶点的序号：");
+        scanf("%d %d", &i, &j);
+
+        //头插法
+        //建立新的邻接点，i为顶点，j为邻接点
+        e = (EdgeNode *)malloc(sizeof(EdgeNode));
+        e->adjvex = j; //邻接序号为j
+        e->next = G->adjList[i].firstedg;
+        G->adjList[i].firstedg = e;
+    }
+}
+
+bool visited[MAXV]; //标识此顶点是否被访问
 /*
     邻接表的深度优先遍历
     思路：
@@ -82,83 +111,273 @@ bool visited[MAXV];//标识此顶点是否被访问
         5.当层递归后辉继续判断 顶点v是否还有未访问的邻接点，有则继续递归
       时间复杂度：O(n+e) 顶点，边数
 */
-void DFS(GraphAdjList G,int v){
-    EdgeNode * p;//迭代边表
-    visited[v] = 1;//置为已访问
-    printf("%d\t",G.adjList[v].data);//输出当前顶点
-    p = G.adjList[v].firstedg;//p指向 v顶点的第一个邻接点
+void DFS(GraphAdjList G, int v)
+{
+    EdgeNode *p;                       //迭代边表
+    visited[v] = 1;                    //置为已访问
+    printf("%d\t", G.adjList[v].data); //输出当前顶点
+    p = G.adjList[v].firstedg;         //p指向 v顶点的第一个邻接点
     while (p)
     {
-        if(!visited[p->adjvex])//判断该邻接点是否被访问
-            DFS(G,p->adjvex);//对此邻接点 调用DFS
-        p = p->next;//p指向 顶点v的下一个邻接点
+        if (!visited[p->adjvex]) //判断该邻接点是否被访问
+            DFS(G, p->adjvex);   //对此邻接点 调用DFS
+        p = p->next;             //p指向 顶点v的下一个邻接点
     }
-    
 }
 
 /*
     邻接表的广度优先遍历
     思路：
         1.建立循环队列
-        2.对数组进行初始化
+        2.对visited数组进行初始化
         3.访问初始顶点，并入队，并置为已访问
-        4.栈不空
-            ·出队，顶点赋值为=给w
+        4.栈不空循环
+            ·出队，顶点赋值给w
             ·p指向w顶点的第一个邻接点链
             ·循环（p）
-                ·判断 p邻接点是否被访问，因循环（p）就对p是否存在进行判断，故无需判断
+                ·判断 p的每一个邻接点是否被访问；   注：因循环（p）就对p是否存在进行判断，故无需判断
                     ·true，输出邻接点，入队，置为已访问
                 ·p迭代
 */
-void BFS(GraphAdjList G,int v){
+void BFS(GraphAdjList G, int v)
+{
     //1
-    EdgeNode * p;
-    int queue[MAXV],rear = 0,front = 0;
-    int w,i;
+    EdgeNode *p;
+    int queue[MAXV], rear = 0, front = 0;
+    int w, i;
     //2
-    for ( i = 0; i < G.numVertex; i++)
+    for (i = 0; i < G.numVertex; i++)
     {
         visited[i] = 0;
     }
     //3
-    printf("%2d",G.adjList[v].data);
+    printf("%2d", G.adjList[v].data);
     visited[v] = 1;
-    rear = (rear+1) % MAXV;
+    rear = (rear + 1) % MAXV;
     queue[rear] = v;
     //4
     while (rear != front)
     {
         //出队
-        front = (front+1) % MAXV;
+        front = (front + 1) % MAXV;
         w = queue[front];
-        //指向栈顶，顶点的第一个邻接点
+        //p指向栈顶，顶点w的第一个邻接点
         p = G.adjList[w].firstedg;
         while (p)
         {
-            if (!visited[p->adjvex])//该顶点是否已访问
+            if (!visited[p->adjvex]) //该顶点是否已访问
             {
                 //输出，入队，置为已访问
-                printf("%2d",p->adjvex);
+                printf("%2d", p->adjvex);
                 visited[p->adjvex] = 1;
-                rear = (rear+1) % MAXV;
+                rear = (rear + 1) % MAXV;
                 queue[rear] = p->adjvex;
             }
-            p = p->next;//p指向栈顶，顶点的下一个邻接点
+            p = p->next; //p指向栈顶，顶点的下一个邻接点
         }
-        
     }
-    
-    
-
 }
 
+//应用
+/*
+    判断图G是否为连通图
+    思路：
+        0.初始化visited
+        1.对该图进行一次深度遍历
+        2.再对该图的所有顶点判断，visited是否都为1
+            ·1，连通
+            ·0，不连通
+*/
+bool connect(GraphAdjList G)
+{
+    bool flag;
+    //1
+    for (int i = 0; i < G.numVertex; i++)
+    {
+        visited[i] = 0;
+    }
+
+    //2
+    DFS(G, 0);
+    for (int i = 0; i < G.numVertex; i++)
+    {
+        //是否都已访问
+        if (visited[i] != 1)
+        {
+            flag = false;
+            break;
+        }
+    }
+    return flag;
+}
+/*
+    邻接表，判断 顶点u 到顶点 v是否存在简单路径
+    思路：
+        1.在深度遍历的基础上加上 u，和v两个参数
+        2.判断u == v ，返回 has 作为是否存在简单路径标识
+*/
+void ExistPath(GraphAdjList G, int u, int v, bool *has)
+{
+    // int w;  //临时存放顶点
+    EdgeNode *p; //迭代邻接点
+    //不可对visited数组初始化,导致visited都为0
+    visited[u] = 1; //置为已访问
+    if (u == v)     //说明从开始u顶点，一直沿着路径访问到v，存在着一条路径
+    {
+        *has = true;
+        return;
+    }
+
+    p = G.adjList[u].firstedg; //p指向顶点u的第一个邻接点
+
+    while (p) //遍历p的每一个邻接点
+    {
+        if (!visited[p->adjvex])
+        {
+            ExistPath(G, p->adjvex, v, has);
+        }
+        p = p->next;
+    }
+}
+
+/*
+    邻接表,输出图G中 从顶点u到顶点v的一条简单路径
+    思路:
+        1.利用图的深度优先遍历,附加三个形参,v,path,d； 每一层递归中的path中存放的路径元素都不同
+            ·其中v代表终点v，path 存放u到v所经过的邻接点，d表示path的大小，初值为-1
+            ·遍历过程中，将当前顶点，加入path[]
+            ·满足u == v的终止条件，说明从u 到 v找到了一条简单路径，输出path
+            ·再从顶点u找一个未访问的顶点w，
+                ·若存在这样的顶点，这从w出发继续进行
+                ·若不存在这样的w，说明该顶点u再往下找不到路径，所以置visited[u] = 0，以便顶点u作为其他路径上的顶点，称为恢复环境
+        最后visited[]都归0，退栈过程中u往下无路径
+*/
+void FindPath(GraphAdjList G, int u, int v, int path[], int d)
+{
+    int w, i;
+    EdgeNode *p;
+    visited[u] = 1;
+    d++; //路径长度+1,u加入路径
+    path[d] = u;
+    if (u == v) //找到一条路径
+    {
+        printf("这是一条简单路径：");
+        for (int i = 0; i <= d; i++)
+        {
+            printf("%2d", path[i]);
+        }
+        printf("\n");
+    }
+
+    p = G.adjList[u].firstedg; //p指向u的第一个邻接点
+    while (p)
+    {
+        if (!visited[p->adjvex])
+        {
+            FindPath(G, p->adjvex, v, path, d);
+        }
+        p = p->next;
+    }
+    visited[u] = 0; //恢复环境，说明顶点u往下找不到路径，让u重新恢复未访问状态，使其可以重新使用
+}
+
+/*
+    输出图G中从顶点u到v的长度为1的所有简单路径   每一层递归中的path中存放的路径元素都不同
+    思路：  和输出所有简单路径类似，将输出路径条件改为d = 1 && u == v即可
+        1.
+*/
+void PathAll(GraphAdjList G, int u, int v, int path[], int d)
+{
+    //d表示path中的路径长度，初始为-1
+    int w, i;
+    EdgeNode *p;
+    visited[u] = 1;
+    d++;
+    path[d] = u; //路径长度增1，顶点u加入路径
+    if (u == v && d == 1)
+    {
+        for (int i = 0; i <= d; i++)
+        {
+            printf("%2d", path[i]);
+        }
+        printf("\n");
+    }
+    p = G.adjList[u].firstedg; //p指向u的第一个邻接点
+    while (p)
+    {
+        w = p->adjvex; //w为u的邻接点
+        if (!visited[w])
+        {
+            PathAll(G, w, v, path, d);
+        }
+        p = p->next; //p指向下一个邻接点
+    }
+    visited[u] = 0; //恢复环境，使u顶点可再次使用
+}
+
+/*
+    邻接表，求图中 通过某顶点k的所有简单回路（若存在）。简单回路，指路径上的顶点不重复，但第一个和最后一个相同的路径
+    其中v代表终点v，path 存放u到v所经过的邻接点，d表示path的大小，初值为-1
+    思路：
+        1.利用回溯的深度优先遍历
+        2.从顶点u开始搜索与之相邻的w，若w等于顶点v其初值为（u）且路径长度大于0，表示找到一条回路，输出path数组
+        3.继续搜索顶点u未访问的相邻点查找其他回路
+*/
+void DFSPath(GraphAdjList G, int u, int v, int path[], int d)
+{
+    int w, i;
+    EdgeNode *p;
+    d++;
+    visited[u] = 1;
+    path[d] = u;               //将u访问，且加入路径
+    p = G.adjList[u].firstedg; //p指向顶点u的第一个相邻点
+
+    while (p)
+    {
+        w = p->adjvex; //w为u 的相邻点
+
+        if (w == v && d > 0) //找到一条回路
+        {
+            for (int i = 0; i <= d; i++)
+            {
+                printf("%2d", path[i]);
+            }
+            printf("%d\n",v);
+        }
+        if (!visited[w])
+        {
+            DFSPath(G, w, v, path, d);
+        }
+        p = p->next;
+    }
+    visited[u] = 0;
+}
+void FindCyclePath(GraphAdjList G, int k)
+{
+    int path[MAXV];
+    printf("经过顶点%d的所有回路：\n");
+    DFSPath(G, k, k, path, -1);
+}
 int main(void)
 {
 
     GraphAdjList G;
-    CreateALGraph(&G);
+    CreateALGraph1(&G);
+    for (int i = 0; i < G.numVertex; i++)
+    {
+        visited[i] = 0;
+    }
+    
     // DFS(G,0);
-    BFS(G,0);
+    // BFS(G,0);
+    // bool flag = connect(G);
+    bool exist = false;
+    VertexType path[MAXV];
+    int d = -1;
+    // ExistPath(G,0,3,&exist);
+    // FindPath(G,0,3,path,d);
+    // PathAll(G,0,3,path,d);
+    FindCyclePath(G, 0);
     getchar();
     getchar();
     return 0;
